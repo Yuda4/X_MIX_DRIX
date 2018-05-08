32cf65545d3ffb8aa790b6b549fa7ff6221b823e
Adiel <32271159+MatufA@users.noreply.github.com>
2018-05-08 21:56:54 +0300

    error main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include "Board.cpp"
 
+
 int main() {
 	Board board1{4};  // Initializes a 4x4 board
 	cout << board1 << endl;   /* Shows an empty board:
-								....
-								....
-								....
-								....
-								*/
+	....
+	....
+	....
+	....
+	*/
+	
 	board1[{1,1}]='X';
+
 	board1[{1,2}]='O';
 	cout << board1 << endl;  /* Shows the following board:
-								....
-								.XO.
-								....
-								....
-								*/
+	....
+	.XO.
+	....
+	....
+	*/
 
 	try {
 		board1[{3,4}]='O';   // This should raise an exception
@@ -23,7 +26,7 @@ int main() {
 		cout << "Illegal coordinate: " << ex.theCoordinate() << endl;  // prints "Illegal coordinate: 3,4"
 	}
 
-	board1= '.';     // Fill the entire board with "."
+	board1 = '.';     // Fill the entire board with "."
 	cout << board1 << endl;  /* Shows an empty board, as above */
 	try { 
 		board1 = 'a';        // This should raise an exception
@@ -36,8 +39,7 @@ int main() {
 	} catch (const IllegalCharException& ex) {
 		cout << "Illegal char: " << ex.theChar() << endl;  // "Illegal char: x"
 	}
-
-	Board board2 = board1; 
+	Board board2 = board1;
 	board2[{0,0}] = 'X';
 	cout << board1 << endl;  /* Shows an empty board, as above */
 	cout << board2 << endl;  /* Shows a board with an X at top-left */
@@ -47,6 +49,7 @@ int main() {
 	cout << board2 << endl;  /* Shows a board with an X at top-left */
 	cout << board1 << endl;  /* Shows a board with an X at top-left and O at bottom-right */
 
+	cout << "Good bye!" << endl;
+
 	return 0;
 }
-
