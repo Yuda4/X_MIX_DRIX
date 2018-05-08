5ad47e74d445cec0d74ff54d4565f8d82941ede5
Adiel <32271159+MatufA@users.noreply.github.com>
2018-05-08 23:48:37 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -42,7 +42,7 @@ class Board{
 
     inline ostream& operator<< (ostream& os, Board& b){
 		for(int rows = 0; rows < b.getSize(); rows++){
-			for(int columns = 0; columns < b.getSize; columns++ ){
+			for(int columns = 0; columns < b.getSize(); columns++ ){
 				os << (b.gameBoard[rows][columns]).get_sign();
 			}
 			os << endl;
