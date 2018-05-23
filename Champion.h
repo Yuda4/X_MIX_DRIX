58b21794c34069cbc02fcabff6a976f7dffc3618
Adiel <32271159+MatufA@users.noreply.github.com>
2018-05-24 01:11:04 +0300

    Update Champion.h

--- Champion.h
+++ Champion.h
@@ -3,9 +3,6 @@
 
 class Champion: public Player {
 public:
-	int counter=-1;
-	int x=0;
 	const string name() const override { return "Yehuda and Adiel"; }
-
 	const Coordinate play(const Board& board) override;
 };
