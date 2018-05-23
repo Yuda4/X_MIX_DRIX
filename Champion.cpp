349e87b36d03bd74a7661c8db83b0903e65da6e7
Adiel <32271159+MatufA@users.noreply.github.com>
2018-05-24 01:10:40 +0300

    Win WIn

--- Champion.cpp
+++ Champion.cpp
@@ -1,16 +1,16 @@
 #include "Champion.h"
-const Coordinate Champion::play(const Board& board) { 
-	if((counter*-1+1)%2==0){
-		Coordinate c{board.size()+counter,x};
-		if (board[c]=='.') return c;
-		
-	}else{
-		Coordinate c{x,board.size()+counter};
-		if (board[c]=='.'){
-		x+=1;
-		counter-=1;
-		return c;	
-		} 
+const Coordinate Champion::play(const Board& board) {
+	Coordinate c{board.size()-1,0};
+	if(board[c]=='.') return c;
+	else{
+		Coordinate c{0,board.size()-1};
+		if(board[c]=='.') return c;
+		else{
+			for(int x=1; x<board.size()-1; ++x){
+				Coordinate c{board.size()-x-1,x};
+				if(board[c]=='.') return c;
+			}
+		}
 	}
-	return {0,0};  // did not find an empty square - play on the top-left
+	return {board.size()-1,board.size()-1};  // did not find an empty square - play on the top-left
 }
